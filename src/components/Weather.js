1f730a60a1cd2f42a3ec7dd077afdddfca7d5ea2
galanggg <tugasgalang@gmail.com>
2020-11-06 06:41:26 +0800

    fix: race condition async

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,10 +4,10 @@ import { Box, Spinner } from '@chakra-ui/core'
 import axios from 'axios'
 
 const Weather = ({ weather }) => {
-  const woeid = weather[0].woeid
   const [data, setData] = useState([])
   const [showData, setShowData] = useState(false)
 
+  const woeid = weather[0].woeid
   const fetchData = async () => {
     const response = await axios.get(
       `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}`,
@@ -19,9 +19,9 @@ const Weather = ({ weather }) => {
 
   useEffect(() => {
     fetchData()
+    //eslint-disable-next-line
   }, [weather])
 
-  // if (!data) return <div>Loading..</div>
   const forecast = data.consolidated_weather
   console.log(forecast)
   if (!showData) {
